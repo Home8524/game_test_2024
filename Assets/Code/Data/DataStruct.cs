4b91d15959f58207112b34c3fa2562bfa4d9a51a
Home8524 <88764103+Home8524@users.noreply.github.com>
2024-07-17 18:37:57 +0900

    tile 데이터 json 포맷으로 불러오게 구조작업

---
+++ Assets/Code/Data/DataStruct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[Serializable]
+public class JsonTileData
+{
+    public List<TileData> datas;
+}
+
+[Serializable]
+public class TileData
+{
+    public MoveVector startPos;
+    public bool isRotate;
+    public int rotateVal;
+    public int cnt;
+    public MoveVector movePos;
+}
+
+public class MoveVector
+{
+    public float x;
+    public float y;
+}
---
+++ Assets/Code/Managers/LocalDataManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LocalDataManager
+{
+    string localPath = Application.dataPath + "/Resources/json";
+
+    private bool Load<T>(string fileName, out T data)
+    {
+        string path = $"{string.Format(localPath, fileName)}";
+        if (File.Exists(path) == false)
+        {
+            data = default;
+            return false;
+        }
+
+        string contents = File.ReadAllText(path);
+        data = JsonUtility.FromJson<T>(contents);
+        return true;
+    }
+
+    public List<TileData> LoadTileData()
+    {
+        Load("stage1", out JsonTileData data);
+        return data.datas;
+    }
+}
